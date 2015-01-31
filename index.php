7c1251975e1cef03dd59be0859e10076907145b8
Vivian Zhang <vivianzhang61@users.noreply.github.com>
2015-01-30 22:30:09 -0800

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 <img src="http://i.imgur.com/GLOwF.jpg" alt="Piggy">
 
 <div id="fb-root"></div>
+
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
