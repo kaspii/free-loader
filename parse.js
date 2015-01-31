88cea8d33a5052cada431ceb07e8d570a7c6e4a2
Ruth Johnson <ruthjohnson2013@gmail.com>
2015-01-31 11:50:31 -0800

    Update parse.js

--- parse.js
+++ parse.js
@@ -1,1 +1,21 @@
-//
+
+var indicator = false;
+function parse(indicator, text){
+var word = "Free";
+var keyword = [];
+
+// Look for "F" in the text
+for(var i = 0; i < text.length; i++) {
+	if (text[i] == word[i]) {
+		// If we find it, add characters up to
+		// the length of my name to the array
+		for(var j = i; j < (word.length + i); j++) {
+		keyword.push(text[j]);
+		}
+	}
+}
+
+if (keyword.length != 0) {
+	indicator = true;
+}
+}
