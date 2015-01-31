14aa8337a68df05d2eca8da80126310221a13f8b
Vivian Zhang <vivianzhang61@users.noreply.github.com>
2015-01-31 12:04:00 -0800

    Update parse.js

--- parse.js
+++ parse.js
@@ -2,20 +2,21 @@
 var indicator = false;
 function parse(indicator, text){
 var word = "free";
-var keyword = [];
 
 // Look for "F" in the text
 for(var i = 0; i < text.length; i++) {
-	if (text[i] == word[i]) {
+	if (text[i] == word[0]) {
+		var j=1;
+		i++;
 		// If we find it, add characters up to
 		// the length of my name to the array
-		for(var j = i; j < (word.length + i); j++) {
-		keyword.push(text[j]);
+		for(i; i < (word.length + i); i++;j++) {
+			if(text[i]==word[j])
+			;
+			else
+			break;
 		}
+		if(j==4)
+		indicator=true;
 	}
-}
-
-if (keyword.length != 0) {
-	indicator = true;
-}
-}
+}}
