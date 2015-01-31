dc62fa55c02debc314b16c51447de7012e686f98
Vivian Zhang <vivianzhang61@users.noreply.github.com>
2015-01-30 23:06:53 -0800

    Update getInfo.js

--- getInfo.js
+++ getInfo.js
@@ -1,12 +1,16 @@
 
-FB.api(
-    "/me",
-    function (response) {
-      if (response && !response.error) {
-     //GET graph.facebook.com
-       // /me?
-    fields=albums.limit(5),posts.limit(5)
-        console.log("hi");
-      }
-    }
-);
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '341086482753224',
+      xfbml      : true,
+      version    : 'v2.2'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
