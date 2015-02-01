a533ea9d76d482d9bb54a3d3cd9ffb466c835294
Vivian Zhang <vivianzhang61@users.noreply.github.com>
2015-02-01 00:33:08 -0800

    Update getInfo.js

--- getInfo.js
+++ getInfo.js
@@ -143,7 +143,8 @@ function traceEvent(id,domain)
 	  	/////////////////////
 	  	console.log("printed");
 	  	console.log(response.name);
-	  	str= "<div class='row'><div class='col-sm-6 col-md-10'><div class='thumbnail'><p id='vivian'></p><div class='caption'><h3>" +response.name+"</h3><p>"+response.description+"</p><p><a href='#' class='btn btn-primary' role='button'>Button</a> </p></div></div></div></div>";
+	  	//str= "<div class='row'><div class='col-sm-6 col-md-10'><div class='thumbnail'><p id='vivian'></p><div class='caption'><h3>" +response.name+"</h3><p>"+response.description+"</p><p><a href='#' class='btn btn-primary' role='button'>Button</a> </p></div></div></div></div>";
+	  	str="why is nothing working?";
 	  	///////////////////////<div onload='getCover("+id+")'class='thumbnail'>
 		}
 		document.getElementById(domain).innerHTML+=str;
