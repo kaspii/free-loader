c6c993afe9fca347bbe1e012071a1aac8f73b5a2
Vivian Zhang <vivianzhang61@users.noreply.github.com>
2015-01-31 18:38:42 -0800

    Update getInfo.js

--- getInfo.js
+++ getInfo.js
@@ -139,8 +139,9 @@ function traceEvent(id)
 	  //	str +="<b>Description: </b>"+response.description+"<br>";
 	  	
 	  	/////////////////////
-	  	str= "<div class='row'><div class='col-sm-6 col-md-10'><div class='thumbnail'><img src="+getCover(id)+" alt='...'><div class='caption'><h3>" +response.name+"</h3><p>"+response.description+"</p><p><a href='#' class='btn btn-primary' role='button'>Button</a> <a href='#' class='btn btn-default' role='button'>Button</a></p></div></div></div></div>";
+	  	str= "<div class='row'><div class='col-sm-6 col-md-10'><div class='thumbnail'><p id=vivian></p><div class='caption'><h3>" +response.name+"</h3><p>"+response.description+"</p><p><a href='#' class='btn btn-primary' role='button'>Button</a> <a href='#' class='btn btn-default' role='button'>Button</a></p></div></div></div></div>";
 	  	///////////////////////
+	  	getCover(id);
 		}
 		document.getElementById("forya").innerHTML+=str;
 	});
@@ -149,7 +150,7 @@ function traceEvent(id)
 function getCover(id)
 {
 	FB.api('/'+id+'?fields=cover', function(response)
-		return response.source;
+		document.getElementById("vivian").innerHTML+="<img src="+response.source+"alt='...'>";
 	)
 }
 /////I don't know what it is. Load the SDK asynchronously
