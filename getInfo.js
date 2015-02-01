4287bc178ac88e6f08019a562d5c9ad64d273b56
Ruth Johnson <ruthjohnson2013@gmail.com>
2015-01-31 18:32:18 -0800

    Update getInfo.js

--- getInfo.js
+++ getInfo.js
@@ -139,7 +139,7 @@ function traceEvent(id)
 	  //	str +="<b>Description: </b>"+response.description+"<br>";
 	  	
 	  	/////////////////////
-	  	str= "<div class='row'><div class='col-sm-6 col-md-10'><div class='thumbnail'><img src='"+response.cover.source+"' alt='...'><div class='caption'><h3>" +response.name+"</h3><p>"+response.description+"</p><p><a href='#' class='btn btn-primary' role='button'>Button</a> <a href='#' class='btn btn-default' role='button'>Button</a></p></div></div></div></div>";
+	  	str= "<div class='row'><div class='col-sm-6 col-md-10'><div class='thumbnail'><img src="+response.cover.source+" alt='...'><div class='caption'><h3>" +response.name+"</h3><p>"+response.description+"</p><p><a href='#' class='btn btn-primary' role='button'>Button</a> <a href='#' class='btn btn-default' role='button'>Button</a></p></div></div></div></div>";
 	  	///////////////////////
 		}
 		document.getElementById("forya").innerHTML+=str;
