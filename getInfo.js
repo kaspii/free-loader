f4884bd6afade980b0d1a7c9a5a682aa85053653
Vivian Zhang <vivianzhang61@users.noreply.github.com>
2015-01-31 18:40:16 -0800

    Update getInfo.js

--- getInfo.js
+++ getInfo.js
@@ -139,7 +139,7 @@ function traceEvent(id)
 	  //	str +="<b>Description: </b>"+response.description+"<br>";
 	  	
 	  	/////////////////////
-	  	str= "<div class='row'><div class='col-sm-6 col-md-10'><div class='thumbnail'><p id=vivian></p><div class='caption'><h3>" +response.name+"</h3><p>"+response.description+"</p><p><a href='#' class='btn btn-primary' role='button'>Button</a> <a href='#' class='btn btn-default' role='button'>Button</a></p></div></div></div></div>";
+	  	str= "<div class='row'><div class='col-sm-6 col-md-10'><div class='thumbnail'><p id='vivian'></p><div class='caption'><h3>" +response.name+"</h3><p>"+response.description+"</p><p><a href='#' class='btn btn-primary' role='button'>Button</a> <a href='#' class='btn btn-default' role='button'>Button</a></p></div></div></div></div>";
 	  	///////////////////////
 	  	getCover(id);
 		}
@@ -150,7 +150,8 @@ function traceEvent(id)
 function getCover(id)
 {
 	FB.api('/'+id+'?fields=cover', function(response)
-		document.getElementById("vivian").innerHTML+="<img src="+response.source+"alt='...'>";
+		var str ="<img src="+response.source+"alt='...'>"
+		document.getElementById("vivian").innerHTML+=str;
 	)
 }
 /////I don't know what it is. Load the SDK asynchronously
