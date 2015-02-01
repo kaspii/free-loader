304a5d6fbf899b399a3c0c8efb6b95f2c2bb6c56
Vivian Zhang <vivianzhang61@users.noreply.github.com>
2015-01-31 23:41:14 -0800

    Update getInfo.js

--- getInfo.js
+++ getInfo.js
@@ -144,6 +144,7 @@ function traceEvent(id,domain)
 		if(indicatorMust.value == true && indicatorOpt.value== true)
 		{
 	  	/////////////////////
+	  	console.log("printed");
 	  	str= "<div class='row'><div class='col-sm-6 col-md-10'><div onload='getCover("+id+")'class='thumbnail'><p id='vivian'></p><div class='caption'><h3>" +response.name+"</h3><p>"+response.description+"</p><p><a href='#' class='btn btn-primary' role='button'>Button</a> <a href='#' class='btn btn-default' role='button'>Button</a></p></div></div></div></div>";
 	  	///////////////////////
 		}
